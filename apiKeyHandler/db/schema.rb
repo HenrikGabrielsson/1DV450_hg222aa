54cd2e31ebc7260933457436c2e3ce1c9b898301
henrikgabrielsson <hg222aa@student.lnu.se>
2015-02-08 10:59:20 +0000

    postgresql

--- apiKeyHandler/db/schema.rb
+++ apiKeyHandler/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150128141738) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: true do |t|
     t.string   "userName",        null: false
     t.string   "password_digest", null: false
