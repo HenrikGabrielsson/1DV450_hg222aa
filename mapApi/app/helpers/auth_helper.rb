1fd4f526c833212b745a7f7cc4ce87321fdf8df2
Henrik Gabrielsson <hg222aa@student.lnu.se>
2015-02-18 20:11:50 +0000

    callcount increment for key

--- mapApi/app/helpers/auth_helper.rb
+++ mapApi/app/helpers/auth_helper.rb
@@ -1,7 +1,14 @@
 module AuthHelper
   def authenticate_api_key
-    if Key.find_by(key: params[:api_key]).nil?
+    
+    @key = Key.find_by(key: params[:api_key])
+    
+    if @key.nil?
       render json: { error: "Det gick inte att autentisera dig." }, status: :unauthorized 
+    
+    else
+      @key.callCount += 1
+      @key.save
     end
   end
     
