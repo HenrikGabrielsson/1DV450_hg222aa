33892a91271dc838142a98b08facd481ce0e54fe
Henrik Gabrielsson <hg222aa@student.lnu.se>
2015-02-21 10:55:19 +0000

    migration fix

--- mapApi/db/migrate/20150215112959_create_tags.rb
+++ mapApi/db/migrate/20150215112959_create_tags.rb
@@ -3,8 +3,6 @@ class CreateTags < ActiveRecord::Migration
     create_table :tags do |t|
       t.string :tag, null: false, limit: 50
 
-      t.belongs_to :memory
-
       t.timestamps
     end
   end
--- mapApi/db/schema.rb
+++ mapApi/db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 20150216211241) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "tag",        limit: 50, null: false
-    t.integer  "memory_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
